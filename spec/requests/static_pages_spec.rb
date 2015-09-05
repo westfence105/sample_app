90d74fc2300224204c85b6ffc0bf1746afa34e83
Togo Nishigaki <westfence105@yahoo.co.jp>
2015-09-06 08:58:32 +0900

    Add about page and its test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
   describe "Home Page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -16,4 +17,13 @@ describe "Static Pages" do
       expect(page).to have_content('Help')
     end
   end
+
+  describe "About Page" do
+    it "should have the content 'About Us'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+
 end
