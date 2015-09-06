aa5ed3da829f14135cb9f6db20b52ec7b87bf719
Togo Nishigaki <westfence105@yahoo.co.jp>
2015-09-06 10:30:29 +0900

    Improve home page and fixed test environment

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe "Static Pages" do
 
   describe "Home Page" do
-    it "should have the title 'Home'" do
+    it "should have the base title" do
     	visit '/static_pages/home'
-    	expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    	expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+    end
+
+    it "should not have a custom page title" do
+        visit '/static_pages/home'
+        expect(page).not_to have_title('| Home')
     end
 
     it "should have the content 'Sample App'" do
